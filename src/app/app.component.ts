045cc887eba211cde327aad4bd9d0c1442c66ee6
Vitor Lamartine <vitorlamartine21@gmail.com>
2023-11-12 05:57:02 -0300

    home page and angular update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@services/auth.service';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Way';
+  isLogged: boolean = false;
+  sidebarOpen: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.authService.loggedIn.subscribe({
+      next: (loggedIn) => {
+        this.isLogged = loggedIn;
+      },
+    });
+
+    this.breakpointObserver
+      .observe(['(max-width: 768px)'])
+      .subscribe((result: BreakpointState) => {
+        this.sidebarOpen = !result.matches;
+      });
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from '@services/auth.service';
 import { HomeComponent } from './shared/home/home.component';
+import { NoteCardComponent } from './shared/home/note-card/note-card.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NoteCardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  isLogged: boolean = false;
-
-  constructor(private authService: AuthService) {
-    this.authService.loggedIn.subscribe({
-      next: (loggedIn) => {
-        this.isLogged = loggedIn;
-      },
-    });
-  }
+  constructor() {}
 }
